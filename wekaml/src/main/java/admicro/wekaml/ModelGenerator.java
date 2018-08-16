f357f3db2428e44f4ebcb7b46ca1a21708f83cb3
dunght <dunghuatri@admicro.vn>
2018-08-16 14:43:54 +0700

    Them ham ve tree va tinh evaluation

--- wekaml/src/main/java/admicro/wekaml/ModelGenerator.java
+++ wekaml/src/main/java/admicro/wekaml/ModelGenerator.java
@@ -1,15 +1,19 @@
 package admicro.wekaml;
 
+import java.awt.BorderLayout;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
 import weka.classifiers.Classifier;
@@ -21,6 +25,8 @@ import weka.core.Instances;
 import weka.core.SerializationHelper;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.converters.TextDirectoryLoader;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
 
 public class ModelGenerator {
 
@@ -42,16 +48,16 @@ public class ModelGenerator {
 	public Classifier buildClassifier(Instances traindataset) {
 		REPTree rt = new REPTree();
 
-		 rt.setBatchSize("64");
+		rt.setBatchSize("64");
 		// rt.setDebug(false);
 		// rt.setDoNotCheckCapabilities(false);
 		// rt.setInitialCount(0);
 		// rt.setMaxDepth(-1);
 		// rt.setMinNum(2);
-		 rt.setMinVarianceProp(0.8);
+		rt.setMinVarianceProp(0.001);
 		// rt.setNoPruning(false);
 		// rt.setNumDecimalPlaces(2);
-		 rt.setNumFolds(10);
+		rt.setNumFolds(10);
 		// rt.setSeed(1);
 		// rt.setSpreadInitialCount(false);
 
@@ -111,32 +117,31 @@ public class ModelGenerator {
 			eval.evaluateModel(model, testdataset);
 		} catch (Exception ex) {
 			Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
-		}		
+		}
 		return eval;
 	}
 
 	public void saveModel(Classifier model, String modelpath) {
 
 		try {
-			SerializationHelper.write(modelpath, model);
+			SerializationHelper.write(modelpath+"/REPTree_model.bin", model);
 		} catch (Exception ex) {
 			Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
-	
-	public Classifier loadModel(String modelpath)
-	{
+
+	public Classifier loadModel(String modelpath) {
 		REPTree model = new REPTree();
 		try {
-			model = (REPTree) SerializationHelper.read(modelpath);
-		} catch (Exception e) {			
+			model = (REPTree) SerializationHelper.read(modelpath+"/REPTree_model.bin");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return model;
 	}
 
 	public void savePredicted(Evaluation eval, String predictedPath) throws IOException {
-		try (Writer writer = Files.newBufferedWriter(Paths.get(predictedPath));
+		try (Writer writer = Files.newBufferedWriter(Paths.get(predictedPath+"/result_score.csv"));
 
 				CSVWriter csvWriter = new CSVWriter(writer, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER,
 						CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);) {
@@ -145,26 +150,153 @@ public class ModelGenerator {
 
 			ArrayList<Prediction> results = eval.predictions();
 			for (int i = 0; i < results.size(); i++) {
-				csvWriter.writeNext(
-						new String[] { Double.toString(results.get(i).predicted()) , Double.toString(results.get(i).actual()) });
+				csvWriter.writeNext(new String[] { Double.toString(results.get(i).predicted()),
+						Double.toString(results.get(i).actual()) });
 			}
 		}
 	}
-	
-	public void saveEvaluation(Evaluation eval, String evalPath)
-	{
-		try (PrintWriter out = new PrintWriter(evalPath)) {			
-		    out.println("Time: "+eval.totalCost()+"\n"+eval.toSummaryString());
-		    try {
-				out.println(eval.toClassDetailsString());
-			} catch (Exception e) {				
+
+	public void saveEvaluation(Evaluation eval, String evalPath) {
+		try (PrintWriter out = new PrintWriter(evalPath+"/eval.txt")) {
+			out.println("Time: " + eval.totalCost() + "\n" + eval.toSummaryString());
+			try {
+				// out.println(eval.toClassDetailsString());
+				List<Double> eval2 = evaluation(evalPath);
+				out.print("TP = "+eval2.get(0)+"\t");
+				out.println("FP = "+eval2.get(1));
+				out.print("FN = "+eval2.get(2)+"\t");
+				out.println("TN = "+eval2.get(3));
+				out.println("-------------------\n");
+				
+				out.println("Accuracy:\t"+eval2.get(4)+"\n");
+				out.println("Class\t\t Precision\t\t\t Recall\t\t\t\t\t F1");
+				out.println("1\t\t "+eval2.get(5)+"\t\t"+eval2.get(6)+"\t\t"+eval2.get(7));
+				out.println("0\t\t "+eval2.get(8)+"\t\t"+eval2.get(9)+"\t\t"+eval2.get(10));
+				out.println("AVG\t\t "+eval2.get(11)+"\t\t"+eval2.get(12)+"\t\t"+eval2.get(13));
+			} catch (Exception e) {
 				e.printStackTrace();
-			}		    
-		} catch (FileNotFoundException e) {			
-			e.printStackTrace();			
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
 
+	public void showTree(REPTree model, String treePath) throws Exception {
+		// display classifier
+		final javax.swing.JFrame jf = new javax.swing.JFrame("Weka Classifier Tree Visualizer: REPTree");
+		jf.setSize(500, 400);
+		jf.getContentPane().setLayout(new BorderLayout());
+		TreeVisualizer tv = new TreeVisualizer(null, model.graph(), new PlaceNode2());
+		jf.getContentPane().add(tv, BorderLayout.CENTER);
+		jf.addWindowListener(new java.awt.event.WindowAdapter() {
+			public void windowClosing(java.awt.event.WindowEvent e) {
+				jf.dispose();
+			}
+		});
+		jf.setVisible(true);
+		tv.fitToScreen();
+		// save tree to file text
+		try (PrintWriter out = new PrintWriter(treePath+"/tree.dot")) {
+			out.println(model.graph());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void convertScoreToLabel(String scorePath, String labelPath, double cut_off) {
+		try (
+				Reader reader = Files.newBufferedReader(Paths.get(scorePath+"/result_score.csv"));				
+				CSVReader csvReader = new CSVReader(reader);
+				Writer writer = Files.newBufferedWriter(Paths.get(labelPath+"/result_label.csv"));
+				CSVWriter csvWriter = new CSVWriter(writer, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER,
+						CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);				
+		) {
+			// Reading Records One by One in a String array
+			String[] nextRecord;
+			nextRecord = csvReader.readNext();
+			String[] headerRecord = {"Predicted","Actual"};
+			csvWriter.writeNext(headerRecord);
+			while ((nextRecord = csvReader.readNext()) != null) {				
+				String predictedScore = nextRecord[0];
+				String actualScore = nextRecord[1];
+				double pScore = Double.parseDouble(predictedScore);
+				double aScore = Double.parseDouble(actualScore);
+				String predictedLabel = "";
+				String actualLabel = "";
+				if(pScore>=cut_off)				
+					predictedLabel = "1";
+				else predictedLabel = "0";
+				if(aScore>=cut_off)				
+					actualLabel = "1";
+				else actualLabel = "0";
+				csvWriter.writeNext(new String[] { predictedLabel, actualLabel });
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public List<Double> evaluation(String resultPath)
+	{
+		List<Double> eval = new ArrayList<>();
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(resultPath+"/result_label.csv"));
+			CSVReader csvReader = new CSVReader(reader);
+			// Reading All Records at once into a List<String[]>
+			int TP = 0;
+			int FP = 0;
+			int TN = 0;
+			int FN = 0;
+			List<String[]> records = csvReader.readAll();
+			for (String[] record : records) {
+			   String predicted = record[0];
+			   String actual = record[1];
+			   if(predicted.equals("1") && actual.equals("1"))			   
+				   TP = TP + 1;			   
+			   if(predicted.equals("1") && actual.equals("0"))			   
+				   FP = FP + 1;
+			   if(predicted.equals("0") && actual.equals("0"))			   
+				   TN = TN + 1;
+			   if(predicted.equals("0") && actual.equals("1"))			   
+				   FN = FN + 1;
+			}
+			double accuracy = (double) (TP+TN)/(TP+TN+FP+FN);
+			double precisionP = (double) TP/(TP+FP);
+			double recallP = (double) TP/(TP+FN);
+			double f1P = (double) 2*precisionP*recallP/(precisionP+recallP);
+			
+			double precisionN = (double) TN/(TN+FN);
+			double recallN = (double) TN/(TN+FP);
+			double f1N = (double) 2*precisionN*recallN/(precisionN+recallN);
+			
+			double precisionAvg = (precisionP+precisionN)/2;
+			double recallAvg = (recallP+recallN)/2;
+			double f1Avg = (f1P+f1N)/2;
+			
+			eval.add((double)TP);
+			eval.add((double)FP);
+			eval.add((double)FN);
+			eval.add((double)TN);
+			
+			eval.add(accuracy);
+			eval.add(precisionP);
+			eval.add(recallP);
+			eval.add(f1P);
+			eval.add(precisionN);
+			eval.add(recallN);
+			eval.add(f1N);
+			
+			eval.add(precisionAvg);
+			eval.add(recallAvg);
+			eval.add(f1Avg);
+			
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+		return eval;
+	}
+	
 	public static void main(String[] args) throws Exception {
 
 		ModelGenerator mg = new ModelGenerator();
--- wekaml/src/main/java/admicro/wekaml/StartWeka.java
+++ wekaml/src/main/java/admicro/wekaml/StartWeka.java
@@ -4,7 +4,18 @@
 package admicro.wekaml;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
+import guru.nidi.graphviz.attribute.Color;
+import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.MutableGraph;
+import guru.nidi.graphviz.parse.Parser;
+
+import static guru.nidi.graphviz.model.Factory.*;
 import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.trees.REPTree;
 import weka.core.Debug;
@@ -20,11 +31,14 @@ import weka.filters.unsupervised.attribute.Normalize;
 public class StartWeka {
 
 //	public static final String DATASETPATH = "data/iris.2D.arff";
-//	public static final String DATASETPATH = "data/heart.csv";	
-	public static String DATASETPATH = "data/heart.arff";
-	public static String MODElPATH = "model/model.bin";
-	public static String RESULTPATH = "result/result.csv";
-	public static String EVALPATH = "result/eval.txt";
+//	public static final String DATASETPATH = "data/heart.csv";
+//	public static String DATASETPATH = "C:/Users/ADMIN/Desktop/Demo/data/features_graph_event.csv";
+//	public static String DATASETPATH = "C:/Users/ADMIN/Desktop/Demo/data/features_graph_topic.csv";
+	public static String DATASETPATH = "C:/Users/ADMIN/Desktop/Demo/data/features_graph_ne.csv";
+	public static String MODElPATH = "model";
+	public static String RESULTPATH = "result";
+	public static String EVALPATH = "result";
+	public static String TREEPATH = "result";
 	
 	public static void runWeka() throws Exception
 	{
@@ -41,7 +55,8 @@ public class StartWeka {
 		startTime = System.currentTimeMillis();
 		//-------------------------------------------//
 		Preporcess prep = new Preporcess();
-		Instances preprocessedData = prep.Numeric2Nominal(originalData,"last");
+//		Instances preprocessedData = prep.Numeric2Nominal(originalData,"last");
+		Instances preprocessedData = originalData;
 		//-------------------------------------------//
 		endTime = System.currentTimeMillis();
 		totalTime = endTime-startTime;
@@ -50,7 +65,7 @@ public class StartWeka {
 		Filter filter = new Normalize();
 
 		// divide dataset to train dataset 80% and test dataset 20%
-		int trainSize = (int) Math.round(preprocessedData.numInstances() * 0.8);
+		int trainSize = (int) Math.round(preprocessedData.numInstances() * 0.6);
 		int testSize = preprocessedData.numInstances() - trainSize;
 		
 		
@@ -125,6 +140,7 @@ public class StartWeka {
 		startTime = System.currentTimeMillis();
 		//-------------------------------------------//
 		mg.savePredicted(eval, RESULTPATH);
+		mg.convertScoreToLabel(RESULTPATH, RESULTPATH, 0.6);
 		//-------------------------------------------//
 		endTime = System.currentTimeMillis();
 		totalTime = endTime-startTime;
@@ -139,14 +155,41 @@ public class StartWeka {
 		endTime = System.currentTimeMillis();
 		totalTime = endTime-startTime;
 		System.out.println("done "+totalTime/1000+" s");
+		
+		// Save evaluation
+		System.out.println("Showing tree ...");
+		startTime = System.currentTimeMillis();
+		//-------------------------------------------//
+		mg.showTree(loadedModel,TREEPATH);
+		createDotGraph();
+		//-------------------------------------------//
+		endTime = System.currentTimeMillis();
+		totalTime = endTime-startTime;
+		System.out.println("done "+totalTime/1000+" s");
+	}
+	
+	public static void createDotGraph() throws IOException
+	{
+		MutableGraph g = Parser.read(new FileInputStream(TREEPATH+"/tree.dot"));
+		Graphviz.fromGraph(g).width(6000).render(Format.PNG).toFile(new File("result/tree1.png"));
+
+		g.graphAttrs()
+		        .add(Color.WHITE.gradient(Color.rgb("888888")).background().angle(90))
+		        .nodeAttrs().add(Color.WHITE.fill())
+		        .nodes().forEach(node -> node.add(Color.hsv(.7, .3, 1.0), Style.lineWidth(2).and(Style.FILLED)));
+		
+		
+		Graphviz.fromGraph(g).width(6000).render(Format.PNG).toFile(new File("result/tree2.png"));
 	}
 
 	public static void main(String[] args) throws Exception {
-		DATASETPATH = args[0];
-		MODElPATH = args[1];
-		RESULTPATH = args[2];
-		EVALPATH = args[3];
+//		DATASETPATH = args[0];
+//		MODElPATH = args[1];
+//		RESULTPATH = args[2];
+//		EVALPATH = args[3];
+//		TREEPATH = args[4];
 		runWeka();
+//		createDotGraph();
 		System.out.println("Done!");
 	}
 
